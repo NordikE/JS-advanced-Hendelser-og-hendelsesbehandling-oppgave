57f1c33de50c65a36c2ddc9dabf45a85608f1df0
NordikE <erik.b10.kodehode@gmail.com>
2024-02-21 11:46:13 +0100

    Path errors

--- data/soundsJSexample.js
+++ data/soundsJSexample.js
@@ -1,6 +1,6 @@
 export const sounds = [
-  { file: "../data/Sound1.mp3", key: "q" },
-  { file: "../data/Sound2.mp3", key: "w" },
-  { file: "../data/Sound3.mp3", key: "e" },
-  { file: "../data/Sound4.mp3", key: "r" },
+  { file: "./data/Sound1.mp3", key: "q" },
+  { file: "./data/Sound2.mp3", key: "w" },
+  { file: "./data/Sound3.mp3", key: "e" },
+  { file: "./data/Sound4.mp3", key: "r" },
 ];
